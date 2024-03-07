e60f3cb405c3a21a433abb01bb50deeade27bcc3
hilmyyes <irfansahilmy@gmail.com>
2024-03-07 14:52:41 +0700

    Stuck NavBar (Tidak Muncul)

--- app/src/main/java/com/example/adv160421080week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160421080week2/MainActivity.kt
@@ -5,15 +5,16 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navController =
-            (supportFragmentManager.findFragmentById(R.id.hostFragment) as
-                    NavHostFragment).navController
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
     override fun onSupportNavigateUp(): Boolean {
